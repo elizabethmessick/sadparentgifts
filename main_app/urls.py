65fe87a9bc653c0a88af96707191c5cbae71507b
Elizabeth Messick <Elizabeth@MacBook-Pro-2.local>
2018-10-12 10:57:11 -0700

    add authentication and profile page access

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('signup/', views.signup, name='signup'),
+    path('user/<username>/', views.profile, name='profile'),
+
 
 ]
