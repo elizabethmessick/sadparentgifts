c0ae17644ee7f007a0639df2f81ffa0e7eee96d7
Elizabeth Messick <Elizabeth@MacBook-Pro-3.local>
2018-10-18 09:48:04 -0700

    update Gift model with gift time upload order

--- main_app/models.py
+++ main_app/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from datetime import datetime
 
 # Create your models here.
+
 
 class Gift(models.Model):
     description = models.CharField(max_length=150)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     photo_url = models.CharField(
         max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.description} ({self.id}) @{self.photo_url}"
 
     def get_absolute_url(self):
         return reverse('profile', kwargs={'id': 1})
+
+    class Meta:
+        ordering = ['-created_at']
 
 
 class Comment(models.Model):
